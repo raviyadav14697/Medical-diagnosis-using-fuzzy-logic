e52db46a35a9d23827cfd15ad9051f4b07c8faff
maitreyeepaliwal <65955491+maitreyeepaliwal@users.noreply.github.com>
2021-05-07 15:34:38 +0530

    Delete php directory

--- php/dp.php
+++
@@ -1,3 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","diseasedb") or die("couldn't to the  server");
-?>
--- php/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	header('location:../html/disease_prediction_system.php');
-?>
